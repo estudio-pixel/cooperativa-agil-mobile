6e0ad1a7d263ed3ac7382d77f8ef832c23443252
Thiago Zanluca <zanlucathiago@gmail.com>
2025-11-24 20:52:25 -0300

    Revert "fix: status bar"
    This reverts commit 0082ad7fda888ba7dfe652e43af6438a3aa4e700.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import { useAppInitialization } from './src/hooks/useAppInitialization';
 import { AppWebView } from './src/components/AppWebView';
-import { SafeContainer } from './src/components';
 
 const App: React.FC = () => {
   useAppInitialization();
 
-  return (
-    <SafeContainer>
-      <AppWebView />
-    </SafeContainer>
-  );
+  return <AppWebView />;
 };
 
 export default App;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,1 @@
 export * from './AppWebView';
-export * from './SafeContainer';
